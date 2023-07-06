5d9cfa3d55b37bb5da597322607f8493233dc8d4
skylarlol <skdyer26@yahoo.com>
2023-07-05 19:27:49 -0500

    feat/ integrated webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import "../pages/index.css"
 import Card from "../components/Card.js";
 import { openPopUp, closePopUp } from "../utils/utils.js";
 import FormValidator from "../components/FormValidator.js";
